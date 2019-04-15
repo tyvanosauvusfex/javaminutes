ed522d2e5938aa15a4e52b8517bfd4665d6672d3
janavohradnikova <jana.vohradnikova@leveris.com>
2019-04-15 15:46:22 +0200

    First commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Y
+)src/com/leveris/tyvanosauvusfex/Main.java,2/f/2fe07931544b26f75d9d97f3f3aba2a37bd521b4
---
+++ src/com/leveris/tyvanosauvusfex/Main.java
@@ -0,0 +1,10 @@
+package com.leveris.tyvanosauvusfex;
+
+public class Main {
+
+    public static void main(String[] args) {
+         {
+            System.out.println("Ahoj světe!");
+        }
+    }
+}
